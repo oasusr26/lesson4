f77c66c1b1ae22421b864301daf5c12e4bab8651
hiro <takehiro.ishio77@gmail.com>
2017-06-19 00:17:01 +0000

    init lesson4

---
+++ common/footer.php
@@ -0,0 +1,44 @@
+        <footer id="footer">
+            <div class="container">
+                <div class="fnav">
+                    <dl class="creative">
+                        <dt>CM・雑誌掲載依頼</dt>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                    </dl>
+                    <dl class="gallary">
+                        <dt>ギャラリー集</dt>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                    </dl>
+                    <dl class="company">
+                        <dt>採用情報</dt>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                    </dl>
+                    <dl class="ir">
+                        <dt>IR情報</dt>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                        <dd><span class="fnav-right">></span><a href="#">aaaa</a></dd>
+                    </dl>
+                </div>
+            </div><!-- /.fnav -->
+            <div class="copyright">
+                <small>
+                    <p>Copyright 2017 &copy; Gallary&Collection. All Rights Reserved.</p>
+                </small>
+            </div><!-- /.copyright -->
+        </footer><!-- /footer -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/footerFixed.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+    </body>
+</html>
